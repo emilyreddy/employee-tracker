69f66f71c03626d3290fcdee22d4fd604250a9b7
emilyreddy <emreddy@icloud.com>
2022-07-11 20:41:55 -0400

    updated connectino info

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,8 @@
 
+
+DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS department;
-
-
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -13,8 +12,8 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
-    department_id INTEGER,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
@@ -24,5 +23,5 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
-)
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
